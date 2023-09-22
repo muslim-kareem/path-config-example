e60dd781189a2bcd7a35a3250c6451015fa75ceb
Muslim Kareem <muslimkareem@icloud.com>
2023-09-22 10:49:48 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/PathConfiguration.java
@@ -0,0 +1,31 @@
+package com.example.tbdx;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PathConfiguration {
+
+    public static Properties getPropsByFilePath(String url) {
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream(url);
+            prop.load(input);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return prop;
+    }
+}
